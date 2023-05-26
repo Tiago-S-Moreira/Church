30802de34fc5150e7b3961b2b6253e99ea23b77a
Tiago Moreira <tiago_risko@hotmail.com>
2023-05-25 21:04:30 -0300

    Beta

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,11 +8,11 @@ function Section() {
     <div/>
     <div className="videos-button">
         <ul>
-            <li><a href=""><button>Culto de Domingo</button></a></li>
-            <li><a href=""><button>Culto de Jovens</button></a></li>
-            <li><a href=""><button>Devocional JM</button></a></li>
-            <li><a href=""></a><button>Hora da história</button></li>
-            <li><a href=""><button>Noite da Esperança</button></a></li>
+            <li><a href="https://www.youtube.com/watch?v=zjK3yr1tmSQ&list=PL_0iwlJ-do5pGyDwTiDjegsSCeN5G0fzs" target='_blank' rel="noreferrer"><button>Culto de Domingo</button></a></li>
+            <li><a href="https://www.youtube.com/watch?v=S9FSvJzWYOU&list=PL_0iwlJ-do5or4MmlBv5bhurDLQClnft1&index=29" target='_blank' rel="noreferrer"><button>Culto de Jovens</button></a></li>
+            <li><a href="https://www.youtube.com/watch?v=IXSj2GNh8xE&list=PL_0iwlJ-do5pAXjCp8rCJgv-u-Dchuqxm" target='_blank' rel="noreferrer"><button>Devocional JM</button></a></li>
+            <li><a href="https://www.youtube.com/watch?v=FPsrs2rjwWI&list=PL_0iwlJ-do5p4CtXJpOahJmdAOPpClKYP&index=55" target='_blank' rel="noreferrer"><button>Hora da história</button></a></li>
+            <li><a href="https://www.youtube.com/watch?v=vgEdqkp4sIg&list=PL_0iwlJ-do5ojITw52X4kmW1jCdfeox56&index=29" target='_blank' rel="noreferrer"><button>Noite da Esperança</button></a></li>
         </ul>
         </div>
     </div>
